3f7dbf9f7ba9395fdb09437efad0c308301f4644
Tauseef Aziz <tachief@outlook.com>
2018-09-27 21:50:14 -0500

    added code for hw2_q4_b.c

--- src/q4/hw2_q4_b.c
+++ src/q4/hw2_q4_b.c
@@ -2,17 +2,32 @@
 #include <stdlib.h>
 #include <omp.h>
 
+//s is the number of iterations
 double MonteCarlo(int s)
 {
-//Write your code here
+    int radius = 1;  //arbitrarily set radius to 1
+    int totalInnerPoints = 0;
+    int i;
+    #pragma omp parallel for reduction(+:totalInnerPoints)
+    for(i = 0; i < s; i++) {
+        double x = (double) rand() / RAND_MAX;
+        double y = (double) rand() / RAND_MAX;
+        //in this case radius = 1 but wanted to represent actual formula 
+        if(x * x + y * y < radius * radius) { 
+            totalInnerPoints++;
+        }
+    }
+
+    return 4.0 * totalInnerPoints / s;
 }
 
 void main()
 {
-double pi;
-pi=MonteCarlo(100000000);
-printf("Value of pi is: %lf\n",pi);
+    double pi;
+    pi=MonteCarlo(100000000);
+    printf("Value of pi is: %lf\n",pi);
 }
+
 
 
 
